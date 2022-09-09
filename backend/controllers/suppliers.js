58943da402cf5d77b63b7a900398f20622312175
Yulia Komogortseva <komog-y-v@yandex.ru>
2022-09-10 04:16:23 +0800

    сделала 2 фугкции

--- backend/controllers/suppliers.js
+++ backend/controllers/suppliers.js
@@ -1,14 +1,15 @@
 const models = require("../models");
 const { Sequelize } = require("sequelize");
 const sequelize = require("../db");
+const  { format, compareAsc } =require( 'date-fns');
+
 
 class Suppliers {
   
   async getPriceDynamics(req, res) {
 
     if (!req.body) return response.sendStatus(400);
-    console.log(req.query);
-
+   
     const list = await sequelize.query(
       `SELECT c.contract_date AS date , quantity AS count
       FROM contract_to_cte
@@ -21,39 +22,65 @@ class Suppliers {
         type: Sequelize.QueryTypes.SELECT,
       } 
     );
-
-    // var priceDynamics= list.map()
-    
-    // {
-    //   data: , 
-    //   value: ,
-    //   type:
-    // }
-    
-    // for (let i = 0; i < .length; i++) {
-      
-    // }
-    // let new_prices = prices.map(price => price * 1.06);
-
     return res.json(list);
   }
 
-  async getContracts(req, res) {
+  async getContractsSpecifications(req, res) {
     if (!req.body) return response.sendStatus(400);
-    
-    return res.json(courseCount);
+    const list = await sequelize.query(
+      `		   
+      select d.value, count(*)
+      from cte 
+      join contract_to_cte c on c.cte_id=cte.id
+      join contracts b on contract_id=b.id
+      join cte_to_characteristics d on d.cte_id=cte.id
+      where cte.category=? and d.name=?
+      group by d.value
+      order by 2 desc`,
+    {
+      replacements: [req.query.category,req.query.name],
+      type: Sequelize.QueryTypes.SELECT,
+    } 
+  );
+    return res.json(list);
   }
+
 
   async getPopularProducts(req, res) {
-    if (!req.body) return response.sendStatus(400);
-    
-    return res.json(courseCount);
-  }
 
-  async getPopularProductsCategories(req, res) {
     if (!req.body) return response.sendStatus(400);
-    
-    return res.json(courseCount);
+
+      ////////////////////////////////////////////переделать
+      const date = new Date();
+      const year = date.getFullYear();
+      let month = date.getMonth() + 1;
+      let f = new Date(year, month, 1).getDate();
+      let l = new Date(year, month, 0).getDate();
+  
+      f = f < 10 ? '0'+f : f;
+      l = l < 10 ? '0'+l : l;
+      month = month < 10 ? '0'+month : month;
+      const firstDay = new Date(`${year-1}-${month}-${f}`);
+      const lastDay = new Date(`${year-1}-${month}-${l}`);
+  
+    const list = await sequelize.query(
+        `		   
+        select title,count(*)
+        from cte 
+        join contract_to_cte c on c.cte_id=cte.id
+        join contracts b on contract_id=b.id
+        where b.contract_date > ? and b.contract_date < ?
+        group by title 
+        order by 2 desc
+        limit 5`,
+      {
+        replacements: [format(firstDay, 'dd.MM.yyyy'),format(lastDay, 'dd.MM.yyyy')],
+        type: Sequelize.QueryTypes.SELECT,
+      } 
+    );
+
+   
+    return res.json(list);
   }
 }
 module.exports = new Suppliers();
--- backend/routes.js
+++ backend/routes.js
@@ -10,16 +10,9 @@ router.get("/categories", categories.get);
 router.get("/suppliers/categories/dynamics", suppliers.getPriceDynamics);
 
 //по проданным товарам выбранной категории ,разделенным по характеристикам
-router.put("/suppliers/categories/contractsSpecifications", suppliers.getContracts);
+router.get("/suppliers/contractsSpecifications", suppliers.getContractsSpecifications);
 
 //популярных за месяц 5 товаров у поставщиков по всей платформе
 router.get("/suppliers/popularProducts", suppliers.getPopularProducts);
-
-//популярных за месяц 5 товаров у поставщиков по выбранной категории
-router.get("/suppliers/categories/popularProducts", suppliers.getPopularProductsCategories);
-
-
-//популярных за месяц товаров поставщика
-//router.get("/categories", categories.get);
 
 module.exports = router;
