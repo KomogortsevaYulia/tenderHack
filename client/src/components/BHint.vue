fd2b21e3632a128190e501ce527db408385b24f9
m <mmailm@mail.ru>
2022-09-11 09:21:50 +0800

    fix

---
+++ client/src/components/BHint.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {Tooltip} from 'bootstrap'
+import {onBeforeUnmount, onMounted, onUnmounted, ref} from "vue";
+
+const elRef = ref()
+const props = defineProps({
+    'tip': {
+        type: String,
+        required: true
+    }
+})
+
+let tooltip = null;
+onMounted(() => {
+    tooltip = new Tooltip(elRef.value)
+})
+
+onBeforeUnmount(() => {
+    tooltip.dispose();
+})
+</script>
+
+
+<template>
+    <i class="fa-solid fa-circle-question" ref="elRef" :data-bs-title="tip"></i>
+</template>
+
+<style scoped>
+    i {
+        cursor: pointer;
+    }
+</style>
